525fa638c05b2dd75c8bf437089a83b6648dba5b
kwak <sal091625@gmail.com>
2025-06-16 20:29:03 +0900

    feat(#3): 회원 리포지토리 테스트 케이스 작성

---
+++ hello-sping/src/test/java/hello/hello_sping/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,59 @@
+package hello.hello_sping.repository;
+
+import hello.hello_sping.domain.Member;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach //각 테스트가 끝날 때마다 repository store 비움
+    public void afterEach(){
+        // 테스트는 실행 순서 상관 없이 독립적으로 동작해야 함 -> 매 테스트가 끝날 때마다 repository를 비움
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("testName");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("member1Name");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("member2Name");
+        repository.save(member2);
+
+        Member result = repository.findByName("member2Name").get();
+        assertThat(member2).isEqualTo(result);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("member1Name");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("member2Name");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+
+    }
+}
