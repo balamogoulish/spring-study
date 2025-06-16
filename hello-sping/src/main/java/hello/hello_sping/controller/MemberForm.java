956622c58b8fb2fcce08b44b16da56883186f0e1
kwak <sal091625@gmail.com>
2025-06-16 23:16:48 +0900

    feat(#5): 회원 웹 기능 - 등록

---
+++ hello-sping/src/main/java/hello/hello_sping/controller/MemberForm.java
@@ -0,0 +1,12 @@
+package hello.hello_sping.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
