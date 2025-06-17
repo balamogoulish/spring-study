5cc0d95b8c518e7ab96bf9322c5ee47bafaee4a1
kwak <sal091625@gmail.com>
2025-06-17 12:55:34 +0900

    feat(#7): 스프링 통합 테스트

--- hello-sping/src/main/java/hello/hello_sping/SpringConfig.java
+++ hello-sping/src/main/java/hello/hello_sping/SpringConfig.java
@@ -1,10 +1,13 @@
 package hello.hello_sping;
 
+import hello.hello_sping.repository.JdbcMemberRepository;
 import hello.hello_sping.repository.MemberRepository;
 import hello.hello_sping.repository.MemoryMemberRepository;
 import hello.hello_sping.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 수동 스프링 빈 등록
 /**
@@ -13,14 +16,20 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ hello-sping/src/test/java/hello/hello_sping/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,102 @@
+package hello.hello_sping.service;
+
+import hello.hello_sping.domain.Member;
+import hello.hello_sping.repository.MemberRepository;
+import hello.hello_sping.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void join() {
+        //given: 주어진 조건
+        Member member = new Member();
+        member.setName("member2Test");
+
+        //when: 실행
+        Long saveId = memberService.join(member);
+
+        //then: 그러면
+        Member result = memberRepository.findById(saveId).get();
+        assertThat(saveId).isEqualTo(result.getId());
+    }
+
+    @Test
+    void join_duplicated() {
+        //given: 주어진 조건
+        Member member1 = new Member();
+        member1.setName("memberTest_duplicated");
+        Member member2 = new Member();
+        member2.setName("memberTest_duplicated");
+
+        //when: 실행
+        Long member1Id = memberService.join(member1);
+        //then: 그러면
+//        try {
+//            Long member2Id = memberService.join(member2);
+//            fail("중복 회원가입으로 예외가 발생해야 합니다.");
+//        } catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+        
+        //예외가 정상적으로 발생하는 지 확인함
+        IllegalStateException e = assertThrows(IllegalStateException.class, ()->memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+    @Test
+    void findMembers() {
+        //given
+
+        //when
+        List<Member> result = memberService.findMembers();
+
+        //then
+        assertThat(5).isEqualTo(result.size());
+    }
+
+    @Test
+    void findOne() {
+        //given
+        Long saveId1 = 1L;
+        Long saveId2 = 2L;
+
+        //when
+        Optional<Member> result1 = memberService.findOne(saveId1);
+        Optional<Member> result2 = memberService.findOne(saveId2);
+        Optional<Member> result3 = memberService.findOne(10L);
+
+        if(result1.isEmpty()){
+            fail("member1을 찾을 수 없습니다.");
+        }
+        result1.ifPresent(m->{assertThat(saveId1).isEqualTo(m.getId());});
+
+        if(result2.isEmpty()){
+            fail("member2을 찾을 수 없습니다.");
+        }
+        result2.ifPresent(m->{assertThat(saveId2).isEqualTo(m.getId());});
+
+        result3.ifPresent(m->fail("등록하지 않은 member3가 존재합니다."));
+
+    }
+}
